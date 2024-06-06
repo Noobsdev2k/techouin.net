76ae4f655581d1b6e4345022ecf8fa54a2c7444b
edenNg <99718570+Noobsdev2k@users.noreply.github.com>
2024-06-06 14:04:25 +0700

    store service model

---
+++ services/store/src/databases/models/inventory.model.ts
@@ -0,0 +1,36 @@
+'use strict'
+
+import { model, Schema } from 'mongoose'
+
+const COLLECTION_NAME = 'Inventories'
+const DOCUMENT_NAME = 'Inventory'
+
+const inventorySchema = new Schema(
+  {
+    invenProductId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    },
+    invenLocation: {
+      type: String,
+      default: 'unKnown'
+    },
+    invenStock: {
+      type: Number,
+      required: true
+    },
+    invenShopId: { type: Schema.Types.ObjectId, ref: 'Shop' },
+    invenReservations: { type: Array, default: [] }
+    /**
+     * cartId
+     * stock
+     * createdOn
+     */
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME
+  }
+)
+
+export const InventoryModel = model(DOCUMENT_NAME, inventorySchema)
